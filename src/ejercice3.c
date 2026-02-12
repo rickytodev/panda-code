314d80a8245f95ce6dada2d8a2166baf7115f60e
RicardoVegaG <rikytoloser@gmail.com>
2026-02-11 23:47:30 -0600

    add new config
    Co-Authored-By: Santiagojv07 <257029503+Santiagojv07@users.noreply.github.com>
    Co-Authored-By: SharkiGames1 <131021487+SharkiGames1@users.noreply.github.com>
    Co-Authored-By: jorgelvega <257052758+jorgelvega@users.noreply.github.com>
    Co-Authored-By: Yami2259 <243192842+Yami2259@users.noreply.github.com>

--- src/ejercice3.c
+++ src/ejercice3.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     float num;
 
     printf("Ingresa un numero: ");
     scanf("%f", &num);
 
-    if (num > 0) {
+    if (num > 0)
+    {
         printf("El numero es positivo");
-    } else if (num < 0) {
+    }
+    else if (num < 0)
+    {
         printf("El numero es negativo");
-    } else {
+    }
+    else
+    {
         printf("El numero es cero");
     }
 }
